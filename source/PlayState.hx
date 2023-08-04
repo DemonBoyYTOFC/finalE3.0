aa4d676047051e2a52f82e18db37bfc6b1581c7a
glauber10 <124374135+DemonBoyYTOFC@users.noreply.github.com>
2023-08-04 20:55:21 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2241,14 +2241,16 @@ class PlayState extends MusicBeatState
 		}
 	}
 
-	public function updateScore(miss:Bool = false)
+		public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | port by glauber01 | Misses: ' + songMisses
 		+ ' | Misses: ' + songMisses
+		+ ' | Port by glauber01 | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
+	var curLight:Int = -1;
+	var curLightEvent:Int = -1;
 
 		if(ClientPrefs.scoreZoom && !miss && !cpuControlled)
 		{
