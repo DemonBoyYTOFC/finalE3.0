50a4dbbb8d0796d6bf06c61c84ef7a5b98f88eb8
glauber10 <124374135+DemonBoyYTOFC@users.noreply.github.com>
2023-08-05 09:45:17 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2244,8 +2244,7 @@ class PlayState extends MusicBeatState
 		public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
-		+ ' | Port by glauber01: ' + songMisses
+		+ ' | Port by glauber01 | Misses: ' + songMisses
 		+ ' | Rating: ' + ratingName
 		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
 
